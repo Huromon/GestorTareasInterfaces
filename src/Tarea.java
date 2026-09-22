631da1e347fccb436db72fd3f65aa12e5e81a931
Huromon <hugo.s355067@cesurformacion.com>
2026-09-22 08:46:04 +0200

    crear los getters y setters

--- src/Tarea.java
+++ src/Tarea.java
@@ -26,4 +26,36 @@ public class Tarea {
        }
 
    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isPendiente() {
+        return pendiente;
+    }
+
+    public void setPendiente(boolean pendiente) {
+        this.pendiente = pendiente;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+     comprobarPrioridad(prioridad);
+    }
 }
